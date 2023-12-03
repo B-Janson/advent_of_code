19646b83b715ecf6bcc094d6012493b7758d5d87
Brandon Janson <brandon@canva.com>
2023-12-03 16:29:00 +1100

    mv around

--- 2023/src/day_1.rs
+++ 2023/src/day_1.rs
@@ -1,10 +1,12 @@
-use std::{fs::File, path::Path, io::{self, BufRead}};
+use advent_of_code_2023::util::util::read_lines;
 use self::NumberAsString::*;
 
 const INPUT: &str = "input/day_1.txt";
 
 const ZERO_DIGIT: u8 = b'0';
 const NINE_DIGIT: u8 = b'9';
+
+const DEBUG_PRINT: bool = false;
 
 #[derive(Debug, Clone, Copy)]
 pub enum NumberAsString {
@@ -47,24 +49,24 @@ fn main() {
         let mut sum: usize = 0;
         for line in lines {
             if let Ok(calibration_value) = line {
-                println!("{}", calibration_value);
+                if DEBUG_PRINT {println!("{}", calibration_value);}
                 let calibration_value_bytes = calibration_value.as_bytes();
 
                 let mut left: u8 = 0;
                 for left_idx in 0..calibration_value.len() {
                     let remaining_string = &calibration_value[left_idx..];
-                    println!("{}", remaining_string);
+                    if DEBUG_PRINT {println!("{}", remaining_string);}
 
                     let single_char = calibration_value_bytes[left_idx];
                     if single_char >= ZERO_DIGIT && single_char <= NINE_DIGIT {
-                        println!("Found a digit at {}: {}", left_idx, single_char as char);
+                        if DEBUG_PRINT {println!("Found a digit at {}: {}", left_idx, single_char as char);}
                         left = single_char - ZERO_DIGIT;
                         break;
                     }
                     
                     for num in NumberAsString::iterator() {
                         if remaining_string.starts_with(num.as_str()) {
-                            println!("Found {} at {} ==> {:?}", num.as_str(), left_idx, num as u32);
+                            if DEBUG_PRINT {println!("Found {} at {} ==> {:?}", num.as_str(), left_idx, num as u32);}
                             left = num as u8;
                             break;
                         }
@@ -78,18 +80,18 @@ fn main() {
                 let mut right: u8 = 0;
                 for right_idx in (0..calibration_value.len()).rev() {
                     let remaining_string = &calibration_value[right_idx..];
-                    println!("{}", remaining_string);
+                    if DEBUG_PRINT {println!("{}", remaining_string);}
 
                     let single_char = calibration_value_bytes[right_idx];
                     if single_char >= ZERO_DIGIT && single_char <= NINE_DIGIT {
-                        println!("Found a digit at {}: {}", right_idx, single_char as char);
+                        if DEBUG_PRINT {println!("Found a digit at {}: {}", right_idx, single_char as char);}
                         right = single_char - ZERO_DIGIT;
                         break;
                     }
                     
                     for num in NumberAsString::iterator() {
                         if remaining_string.starts_with(num.as_str()) {
-                            println!("Found {} at {} ==> {:?}", num.as_str(), right_idx, num as u32);
+                            if DEBUG_PRINT {println!("Found {} at {} ==> {:?}", num.as_str(), right_idx, num as u32);}
                             right = num as u8;
                             break;
                         }
@@ -100,8 +102,8 @@ fn main() {
                     }
                 }
 
-                println!("left {} right {}", left, right);
-                println!("{}", left * 10 + right);
+                if DEBUG_PRINT {println!("left {} right {}", left, right);}
+                if DEBUG_PRINT {println!("{}", left * 10 + right);}
                 let line_number: usize = left as usize * 10 + right as usize;
                 sum += line_number;
             }
@@ -113,12 +115,6 @@ fn main() {
     
 }
 
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+
 
 
---
+++ 2023/src/day_2.rs
@@ -0,0 +1,7 @@
+use advent_of_code_2023::util::util::read_lines;
+
+const INPUT: &str = "input/day_2.txt";
+
+fn main() {
+
+}
