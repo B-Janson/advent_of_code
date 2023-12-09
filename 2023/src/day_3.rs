7803e2639b16263ef5f55cc496476e29cb891e83
Brandon Janson <brandon@canva.com>
2023-12-10 10:20:29 +1100

    day 3 p2

--- 2023/src/day_3.rs
+++ 2023/src/day_3.rs
@@ -6,7 +6,6 @@ use std::{
 
 use advent_of_code_2023::util::util::read_lines;
 
-
 const INPUT: &str = "input/day_3.txt";
 
 fn part_one() {
@@ -120,9 +119,7 @@ fn part_one() {
 }
 
 fn part_two() {
-    let symbols: HashSet<char> = vec!['*']
-        .into_iter()
-        .collect();
+    let symbols: HashSet<char> = vec!['*'].into_iter().collect();
     let numbers_as_chars = '0'..='9';
 
     let mut sum: usize = 0;
@@ -143,9 +140,11 @@ fn part_two() {
             }
 
             if numbers_as_chars.contains(&c) {
-                if col == 0 || !numbers_as_chars.contains(row_input.get(col - 1).unwrap()) {
+                if col > 0 && numbers.contains_key(&(row, col - 1)) {
+                    numbers.insert((row, col), *numbers.get(&(row, col - 1)).unwrap());
+                } else {
                     let end_num_idx =
-                        &unwrapped_line[col..].find(|c: char| symbols.contains(&c) || c == '.');
+                        &unwrapped_line[col..].find(|c: char| !numbers_as_chars.contains(&c));
                     let full_number: &str;
                     if let Some(num_idx) = end_num_idx {
                         full_number = &unwrapped_line[col..col + num_idx];
@@ -160,6 +159,9 @@ fn part_two() {
         grid.push(row_input);
     }
 
+    // println!("{:?}", numbers);
+    // println!("{:?}", symbol_locations);
+
     let surrounding: Vec<(i32, i32)> = vec![
         (-1, -1),
         (-1, 0),
@@ -172,61 +174,67 @@ fn part_two() {
         (1, 1),
     ];
 
-    for (grid_location, val) in numbers {
-        for col_iter in 0..num_digits(val) {
-            let new_grid_location = (grid_location.0, grid_location.1 + col_iter);
+    for (star_row, star_col) in symbol_locations {
+        let mut skip: HashSet<usize> = HashSet::new();
+        if star_row == 0 {
+            skip.insert(0);
+            skip.insert(1);
+            skip.insert(2);
+        }
 
-            let mut skip: HashSet<usize> = HashSet::new();
-            if new_grid_location.0 == 0 {
-                skip.insert(0);
-                skip.insert(1);
-                skip.insert(2);
-            }
+        if star_row == grid.len() - 1 {
+            skip.insert(6);
+            skip.insert(7);
+            skip.insert(8);
+        }
 
-            if new_grid_location.0 == grid.len() - 1 {
-                skip.insert(6);
-                skip.insert(7);
-                skip.insert(8);
-            }
+        if star_col == 0 {
+            skip.insert(0);
+            skip.insert(3);
+            skip.insert(6);
+        }
 
-            if new_grid_location.1 == 0 {
-                skip.insert(0);
-                skip.insert(3);
-                skip.insert(6);
-            }
+        if star_col == grid[star_col].len() - 1 {
+            skip.insert(2);
+            skip.insert(5);
+            skip.insert(8);
+        }
 
-            if new_grid_location.1 == grid[new_grid_location.1].len() - 1 {
-                skip.insert(2);
-                skip.insert(5);
-                skip.insert(8);
-            }
+        println!("grid_location: {:?} --> skip: {:?}", (star_row, star_col), skip);
 
-            // println!("grid_location: {:?} --> value:{} --> skip: {:?}", new_grid_location, val, skip);
+        let mut found_numbers: Vec<usize> = Vec::new();
 
-            let mut found_symbol = false;
-
-            for (idx, surrounding_cell) in surrounding.iter().enumerate() {
-                if !skip.contains(&idx) {
-                    let curr_row = surrounding_cell.0 + new_grid_location.0 as i32;
-                    let curr_col = surrounding_cell.1 + new_grid_location.1 as i32;
-                    // println!("{curr_row}, {curr_col}");
-                    let adjusted_idxs = (curr_row as usize, curr_col as usize);
-                    if symbol_locations.contains(&adjusted_idxs) {
-                        found_symbol = true;
-                        // println!("found symbol {:?} for value {} at {:?}", symbol_locations.get(&adjusted_idxs), val, &adjusted_idxs);
-                        break;
-                    }
+        for (idx, surrounding_cell) in surrounding.iter().enumerate() {
+            if !skip.contains(&idx) {
+                let curr_row = surrounding_cell.0 + star_row as i32;
+                let curr_col = surrounding_cell.1 + star_col as i32;
+                println!("{curr_row}, {curr_col}");
+                let adjusted_idxs = (curr_row as usize, curr_col as usize);
+                if numbers.contains_key(&adjusted_idxs) && !found_numbers.contains(numbers.get(&adjusted_idxs).unwrap()) {
+                    println!("found value {} at {:?}", numbers.get(&adjusted_idxs).unwrap(), &adjusted_idxs);
+                    found_numbers.push(*numbers.get(&adjusted_idxs).unwrap());
                 }
-            }
-
-            if found_symbol {
-                sum += val;
-                break;
+                // if symbol_locations.contains(&adjusted_idxs) {
+                //     found_symbol = true;
+                //     // 
+                //     break;
+                // }
             }
         }
+
+        if found_numbers.len() == 2 {
+            let result = found_numbers.get(0).unwrap() * found_numbers.get(1).unwrap();
+            sum += result;
+            println!("Multiplyting {:?} * {:?} = {}", found_numbers.get(0), found_numbers.get(1), result);
+        }
+
+        // if found_symbol {
+        //     sum += val;
+        //     break;
+        // }
     }
 
-    println!("Part one: {}", sum);
+    println!("Part two: {}", sum);
 }
 
 fn num_digits(mut num: usize) -> usize {
@@ -241,6 +249,4 @@ fn num_digits(mut num: usize) -> usize {
 fn main() {
     part_one();
     part_two();
-    // part_one(&game_number_re, &red_cube_count_re, &green_cube_count_re, &blue_cube_count_re);
-    // part_two(&game_number_re, &red_cube_count_re, &green_cube_count_re, &blue_cube_count_re);
 }
